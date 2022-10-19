90de777983b1b5d2e5f1d63ddd7bb18f85afec94
kevwing <kevin.wing@lshtm.ac.uk>
2022-10-19 16:57:11 +0100

    redoing order of covariate management

---
+++ analysis/covariates_complete.py
@@ -0,0 +1,223 @@
+from cohortextractor import filter_codes_by_category, patients, combine_codelists
+from codelists import *
+from datetime import datetime, timedelta
+
+
+def generate_covariates_complete(index_date_variable):
+
+    covariates_complete = dict(
+
+    # DEMOGRAPHICS AND LIFESTYLE 
+
+    # RURAL OR URBAN LOCATION
+    rural_urban=patients.address_as_of(
+        "index_date",
+        returning="rural_urban_classification",
+        return_expectations={
+            "rate": "universal",
+            "category": 
+                {"ratios": {
+                    "1": 0.1,
+                    "2": 0.1,
+                    "3": 0.1,
+                    "4": 0.1,
+                    "5": 0.1,
+                    "6": 0.1,
+                    "7": 0.2,
+                    "8": 0.2,
+                }
+            },
+        },
+    ),
+
+    #ETHNICITY IN 5 CATEGORIES
+    ethnicity=patients.with_these_clinical_events(
+        ethnicity_codes,
+        returning="category",
+        find_last_match_in_period=True,
+        include_date_of_match=True,
+        return_expectations={
+            "category": {"ratios": {"1": 0.25, "2": 0.25, "3": 0.25, "5": 0.25}},
+            "incidence": 0.9,
+        },
+    ),
+
+    #ETHNICITY IN 16 CATEGORIES
+    ethnicity_16=patients.with_these_clinical_events(
+        ethnicity_codes_16,
+        returning="category",
+        find_last_match_in_period=True,
+        include_date_of_match=True,
+        return_expectations={
+            "category": {
+                "ratios": {
+                    "1": 0.0625,
+                    "2": 0.0625,
+                    "3": 0.0625,
+                    "4": 0.0625,
+                    "5": 0.0625,
+                    "6": 0.0625,
+                    "7": 0.0625,
+                    "8": 0.0625,
+                    "9": 0.0625,
+                    "10": 0.0625,
+                    "11": 0.0625,
+                    "12": 0.0625,
+                    "13": 0.0625,
+                    "14": 0.0625,
+                    "15": 0.0625,
+                    "16": 0.0625,
+                }
+            },
+            "incidence": 0.75,
+        },
+    ),
+
+
+
+
+    # COMORBIDITIES (PRIOR DIAGNOSES OF BROAD DIAGNOSTIC CATEGORIES)
+    comorb_infection_or_parasite=patients.with_these_clinical_events(
+        infectious_disease_codes,
+        between=[f"{index_date_variable} - 1 year", f"{index_date_variable}"],
+        returning="binary_flag", 
+        return_expectations={"incidence": 0.15},
+    ),   
+
+    comorb_neoplasms=patients.with_these_clinical_events(
+        neoplastic_disease_codes,
+        between=[f"{index_date_variable} - 1 year", f"{index_date_variable}"],
+        returning="binary_flag", 
+        return_expectations={"incidence": 0.15},
+    ), 
+
+    comorb_blood_diseases=patients.with_these_clinical_events(
+        combine_codelists(
+            blood_cellular_disease_codes,
+            disorder_haematopoietic_codes,
+            disorder_immune_codes,
+        ),
+        between=[f"{index_date_variable} - 1 year", f"{index_date_variable}"],
+        returning="binary_flag", 
+        return_expectations={"incidence": 0.15},
+    ),
+
+    comorb_endocrine_nutr_metab_diseases=patients.with_these_clinical_events(
+        combine_codelists(
+            disorder_endocrine_codes,
+            metabolic_disease_codes,
+            nutritional_disorder_codes,
+        ),
+        between=[f"{index_date_variable} - 1 year", f"{index_date_variable}"],
+        returning="binary_flag", 
+        return_expectations={"incidence": 0.15},
+    ),
+
+    comorb_mental_behav_disorder=patients.with_these_clinical_events(
+        mental_disorder_codes,
+        between=[f"{index_date_variable} - 1 year", f"{index_date_variable}"],
+        returning="binary_flag", 
+        return_expectations={"incidence": 0.15},
+    ),
+
+    comorb_nervous_system_diseases=patients.with_these_clinical_events(
+        combine_codelists(
+            disorder_neurological_codes,
+            cns_codes,
+        ),
+        between=[f"{index_date_variable} - 1 year", f"{index_date_variable}"],
+        returning="binary_flag", 
+        return_expectations={"incidence": 0.15},
+    ),
+
+    comorb_ear_mastoid_disease=patients.with_these_clinical_events(
+        auditory_disorder_codes,
+        between=[f"{index_date_variable} - 1 year", f"{index_date_variable}"],
+        returning="binary_flag", 
+        return_expectations={"incidence": 0.15},
+    ),
+
+    comorb_circulatory_system_disease=patients.with_these_clinical_events(
+        cardiovascular_disease_codes,
+        between=[f"{index_date_variable} - 1 year", f"{index_date_variable}"],
+        returning="binary_flag", 
+        return_expectations={"incidence": 0.15},
+    ),
+
+    comorb_respiratory_system_disease=patients.with_these_clinical_events(
+        disorder_respiratory_codes,
+        between=[f"{index_date_variable} - 1 year", f"{index_date_variable}"],
+        returning="binary_flag", 
+        return_expectations={"incidence": 0.15},
+    ),
+
+    comorb_digestive_system_disease=patients.with_these_clinical_events(
+        disorder_digestive_codes,
+        between=[f"{index_date_variable} - 1 year", f"{index_date_variable}"],
+        returning="binary_flag", 
+        return_expectations={"incidence": 0.15},
+    ),
+
+    comorb_skin_disease=patients.with_these_clinical_events(
+        disorder_skin_codes,
+        between=[f"{index_date_variable} - 1 year", f"{index_date_variable}"],
+        returning="binary_flag", 
+        return_expectations={"incidence": 0.15},
+    ),
+
+    comorb_muscuoloskeletal_connective_diseases=patients.with_these_clinical_events(
+        combine_codelists(
+            disorder_musculoskeletal_codes,
+            disorder_connective_codes,
+        ),
+        between=[f"{index_date_variable} - 1 year", f"{index_date_variable}"],
+        returning="binary_flag", 
+        return_expectations={"incidence": 0.15},
+    ),
+
+    comorb_genitourinary_disease=patients.with_these_clinical_events(
+        disorder_genitourinary_codes,
+        between=[f"{index_date_variable} - 1 year", f"{index_date_variable}"],
+        returning="binary_flag", 
+        return_expectations={"incidence": 0.15},
+    ),
+
+    comorb_pregnancy_complications=patients.with_these_clinical_events(
+        combine_codelists(
+            compl_pregnancy_codes,
+            disorder_peurperium_codes,
+            disorder_labor_codes,
+        ),
+        between=[f"{index_date_variable} - 1 year", f"{index_date_variable}"],
+        returning="binary_flag", 
+        return_expectations={"incidence": 0.15},
+    ),
+
+    comorb_perinatal_disease=patients.with_these_clinical_events(
+        disorder_fetus_codes,
+        between=[f"{index_date_variable} - 1 year", f"{index_date_variable}"],
+        returning="binary_flag", 
+        return_expectations={"incidence": 0.15},
+    ),
+
+    comorb_congenital_disease=patients.with_these_clinical_events(
+        congential_disease_codes,
+        between=[f"{index_date_variable} - 1 year", f"{index_date_variable}"],
+        returning="binary_flag", 
+        return_expectations={"incidence": 0.15},
+    ),
+
+    comorb_injury_poisoning=patients.with_these_clinical_events(
+        combine_codelists(
+            poisoning_codes,
+            injury_codes,
+        ),
+        between=[f"{index_date_variable} - 1 year", f"{index_date_variable}"],
+        returning="binary_flag", 
+        return_expectations={"incidence": 0.15},
+    ), 
+
+
+    
+    )
+    return covariates_complete
--- analysis/study_definition_complete_covid_communitycases.py
+++ analysis/study_definition_complete_covid_communitycases.py
@@ -17,20 +17,11 @@ CASES = "output/input_covid_matched_cases_allSTPs.csv"
 
 # Import Variables 
 
-# Import Variables 
 
-## covid19 variables - don't think I need this as already added in setup stage
-from covid19_variables import generate_covid19_variables
-covid19_variables = generate_covid19_variables(index_date_variable="case_index_date")
-
-## matching variables - as above
-from matching_variables import generate_matching_variables
-matching_variables = generate_matching_variables(index_date_variable="case_index_date")
-
-## covariates: age, sex, region we already have as matching variables, need IMD, ethnicity, rural/urban, pre-existing comorbidity (although this comes from outcome vars)
-## (note, all above relative to community COVID19 case (index) date)
-from covariates import generate_covariates
-covariates = generate_covariates(index_date_variable="case_index_date")
+## covariates not yet added i.e.: rural/urban, ethnicity, comorbidities
+## all above relative to index date
+from covariates_complete import generate_covariates_complete
+covariates_complete= generate_covariates_complete(index_date_variable="case_index_date")
 
 ## outcome variables (note, relative to community COVID19 case (index) date)
 from outcome_variables import generate_outcome_variables
@@ -53,62 +44,119 @@ study = StudyDefinition(
     # start of observation period (note, needs to be called index date)
     index_date="2020-02-01", # note should be ignored when using case_index_date 
 
-    # import the case index date 
+    # import the variables I need that already exist in the matched list of cases
+    # doing it this way as am uncomfortable about "reextracting" any variables that I have already extracted ((and performed checks)
+    # for example, when I re-extracted, there there were some missing STPs and some sex categories other than M or F, despite these
+    # being defined as required (for STP) or only M or F (for sex)
+
+    # age
+    age=patients.with_value_from_file(
+        CASES, 
+        returning="age", 
+        returning_type="int"), 
+    # case
+    case=patients.with_value_from_file(
+        CASES, 
+        returning="case", 
+        returning_type="int"), 
+    # case index date
     case_index_date=patients.with_value_from_file(
         CASES, 
         returning="case_index_date", 
         returning_type="date"), 
+    # covid_hosp
+    covid_hosp=patients.with_value_from_file(
+        CASES, 
+        returning="covid_hosp", 
+        returning_type="str"), 
+    # covid_tpp_prob
+    covid_tpp_prob=patients.with_value_from_file(
+        CASES, 
+        returning="covid_tpp_prob", 
+        returning_type="str"),
+    # covid_tpp_probw2
+    covid_tpp_probw2=patients.with_value_from_file(
+        CASES, 
+        returning="covid_tpp_probw2", 
+        returning_type="str"),
+    # death_date
+    death_date=patients.with_value_from_file(
+        CASES, 
+        returning="death_date", 
+        returning_type="str"),
+    # dereg_date
+    dereg_date=patients.with_value_from_file(
+        CASES, 
+        returning="dereg_date", 
+        returning_type="str"),
+    # first_known_covid19
+    first_known_covid19=patients.with_value_from_file(
+        CASES, 
+        returning="first_known_covid19", 
+        returning_type="str"),
+    # first_pos_test
+    first_pos_test=patients.with_value_from_file(
+        CASES, 
+        returning="first_pos_test", 
+        returning_type="str"),
+    # first_pos_testw2
+    first_pos_testw2=patients.with_value_from_file(
+        CASES, 
+        returning="first_pos_testw2", 
+        returning_type="str"),
+    # had_covid_hosp
+    had_covid_hosp=patients.with_value_from_file(
+        CASES, 
+        returning="had_covid_hosp", 
+        returning_type="int"),
+    # has_died
+    has_died=patients.with_value_from_file(
+        CASES, 
+        returning="has_died", 
+        returning_type="int"),
+    # has_follow_up
+    has_follow_up=patients.with_value_from_file(
+        CASES, 
+        returning="has_follow_up", 
+        returning_type="int"),
+    # imd
+    imd=patients.with_value_from_file(
+        CASES, 
+        returning="imd", 
+        returning_type="int"),
+    # match_counts
+    match_counts=patients.with_value_from_file(
+        CASES, 
+        returning="match_counts", 
+        returning_type="int"),
+    # pos_covid_test_ever
+    pos_covid_test_ever=patients.with_value_from_file(
+        CASES, 
+        returning="pos_covid_test_ever", 
+        returning_type="str"),
+    # set_id
+    set_id=patients.with_value_from_file(
+        CASES, 
+        returning="set_id", 
+        returning_type="float"), 
+    # sex
+    sex=patients.with_value_from_file(
+        CASES, 
+        returning="sex", 
+        returning_type="str"), 
+    # stp
+    stp=patients.with_value_from_file(
+        CASES, 
+        returning="stp", 
+        returning_type="str"),
+    
 
-    # COVID19 VARIABLES
-    **covid19_variables, 
-
-    # MATCHING VARIABLES  
-    **matching_variables, 
-
+    # extract the new variables i.e. (1) covariates: ethnicity, rural_urban and comorbidities and (2) outcomes
     # COVARIATES  
-    **covariates, 
+    **covariates_complete, 
 
     # OUTCOME VARIABLES  
     **outcome_variables,  
-
-
-    # TIME-VARYING SELECTION VARIABLES
-    ## These variables are EITHER 1) defined within a specific time frame relative to the patient case date 
-    ## OR 2) defined as before/after/on the patient case date, but they return something other than a date 
-    ## For controls, they need to be extracted and applied after matching as controls are assigned the patient case date 
-    ## It is most efficient to extract these separately for exposed and controls, as they are not needed for the controls yet,
-    ## and we don't want to spend time matching ineligble exposed people  
-
-    ### has 3 months of of baseline time
-    ## Note that CASE_INDEX_DATE is defined in the covid19_variables files and is the earliest of first positive test or first probable diagnosis in W2
-    has_follow_up=patients.registered_with_one_practice_between(
-        start_date="case_index_date - 3 months",
-        end_date="case_index_date",
-        return_expectations={"incidence": 0.95},
-        ),
-
-    ### died before case index date
-    has_died=patients.died_from_any_cause(
-      on_or_before="case_index_date",
-      returning="binary_flag",
-    ),
-
-    ### died after case index date (extracted as used as a matching variable, so needs to exist)
-    death_date=patients.died_from_any_cause(
-        on_or_after="case_index_date",
-        returning="date_of_death",
-        date_format="YYYY-MM-DD", 
-        return_expectations={
-            "date": {
-                "earliest": "2020-02-01",  
-                "latest": "2021-01-31", }, 
-                "incidence": 0.01 },
-    ),
-
-    ### deregistered after case index date (extracted as used as a matching variable, so needs to exist)
-    dereg_date=patients.date_deregistered_from_all_supported_practices(
-        on_or_after="case_index_date", date_format="YYYY-MM",
-    ),
 
 
 ) 
