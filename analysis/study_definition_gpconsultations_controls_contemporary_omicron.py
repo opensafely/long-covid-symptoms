3108e6e4dc5ce761b6245704182deee6ffbc57c5
Kev Wing <64849534+kevwing@users.noreply.github.com>
2023-11-03 11:00:10 +0000

    controls omicron gp consultation

---
+++ analysis/study_definition_gpconsultations_controls_contemporary_omicron.py
@@ -0,0 +1,77 @@
+# Import necessary functions
+from cohortextractor import (
+    StudyDefinition,
+    patients,
+    codelist_from_csv,
+    codelist,
+    filter_codes_by_category,
+    combine_codelists
+)
+
+# Import all codelists
+from codelists import *
+
+# Import the required data 
+
+CONTROLS = "output/input_covid_matched_matches_contemporary_allSTPs_omicron.csv"
+
+
+
+# Specify study definition
+
+study = StudyDefinition(
+    # configure the expectations framework
+    default_expectations={
+        "date": {"earliest": "1900-01-01", "latest": "today"},
+        "rate": "uniform",
+        "incidence" : 0.2
+    },
+
+     # select the study population
+    population=patients.which_exist_in_file(CONTROLS), 
+
+    # start of observation period (note, needs to be called index date)
+    index_date="2020-02-01", # note should be ignored when using case_index_date 
+
+    # get case index date from original file
+    case_index_date=patients.with_value_from_file(
+        CONTROLS, 
+        returning="case_index_date", 
+        returning_type="date"), 
+
+
+
+    #NUMBER OF GP CONSULTATIONS IN THE PREVIOUS YEAR
+    gp_count_prevyear=patients.with_gp_consultations(
+        between=["case_index_date - 394 days", "case_index_date - 29 days"],
+        returning="number_of_matches_in_period",
+        return_expectations={"int": {"distribution": "normal", "mean": 6, "stddev": 3},"incidence": 0.6,},
+    ),
+
+    #NUMBER OF GP CONSULTATIONS (AS AN OUTCOME)
+    total_gp_count=patients.with_gp_consultations(
+        between=["case_index_date", "2023-03-31"],
+        returning="number_of_matches_in_period",
+        return_expectations={"int": {"distribution": "normal", "mean": 6, "stddev": 3},"incidence": 0.6,},
+    ),  
+
+    t1_gp_count=patients.with_gp_consultations(
+        between=["case_index_date + 28 days", "case_index_date + 84 days"],
+        returning="number_of_matches_in_period",
+        return_expectations={"int": {"distribution": "normal", "mean": 6, "stddev": 3},"incidence": 0.6,},
+    ), 
+
+    t2_gp_count=patients.with_gp_consultations(
+        between=["case_index_date + 85 days", "case_index_date + 180 days"],
+        returning="number_of_matches_in_period",
+        return_expectations={"int": {"distribution": "normal", "mean": 6, "stddev": 3},"incidence": 0.6,},
+    ),      
+
+    t3_gp_count=patients.with_gp_consultations(
+        between=["case_index_date + 181 days", "2023-03-31"],
+        returning="number_of_matches_in_period",
+        return_expectations={"int": {"distribution": "normal", "mean": 6, "stddev": 3},"incidence": 0.6,},
+    ), 
+
+
+) 
